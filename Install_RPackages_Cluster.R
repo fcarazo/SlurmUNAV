0abb401198ab6f2d883960e7bde051c4b72cf578
jgarces <jgarces@unav.es>
2018-01-24 12:20:05 +0100

    Update Install_RPackages_Cluster.R

--- Install_RPackages_Cluster.R
+++ Install_RPackages_Cluster.R
@@ -20,3 +20,9 @@ source("http://bioconductor.org/biocLite.R")
 biocLite("EventPointer", lib=RLibLoc)
 biocLite("limma", lib=RLibLoc)
 biocLite("ComplexHeatmap", lib=RLibLoc)
+
+
+#### NOTE
+### It's possible to add the new location in the .Rprofile and avoid write every time the lib option (for a .Rprofile example, goo.gl/j8Efvv)
+.libPaths("/mnt/beegfs/jgarces/software/r_packages")
+message("Using library: ", .libPaths()[1])
